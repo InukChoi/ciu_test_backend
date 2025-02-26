aa9884a1917a85a5c9f02db0559ff4fdb9e32395
InukChoi <iu.choi3197@gmail.com>
2025-02-26 11:33:09 +0900

    [Feat] 게시글 상세 조회 기능 #4

--- src/main/java/com/example/ciu_test_backend/board/BoardRepository.java
+++ src/main/java/com/example/ciu_test_backend/board/BoardRepository.java
@@ -4,11 +4,13 @@ import com.example.ciu_test_backend.board.model.Board;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
     @Lock(LockModeType.PESSIMISTIC_READ)
+    @Query("select b from Board b join fetch b.comments where b.idx = :idx")
     Optional<Board> findById(Long idx);
 }
